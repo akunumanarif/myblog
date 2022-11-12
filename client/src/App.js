916eb64c3e025ae7683b36bd9f311c7bf02cea0f
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-13 01:45:16 +0700

    idem

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider, Router } from "react-router-dom";
 import Register from "./pages/Register";
-import Register from "./pages/Login";
-import Register from "./pages/Home";
-import Register from "./pages/Api";
-import Register from "./pages/Single";
-import Register from "./pages/Write";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Api from "./pages/Api";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
 const router = createBrowserRouter([
   {
     path: "/",
