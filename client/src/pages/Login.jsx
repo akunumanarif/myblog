a394f1dec67adaec8ebac02a96804c3ad7596548
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-13 02:14:41 +0700

    style and login create

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div>
-            Login Page
+        <div className='auth'>
+
+            <h1>Login</h1>
+            <form >
+                <input type="text" placeholder='username'/>
+                <input type="password" placeholder='password'/>
+                <button>Login</button>
+            </form>
         </div>
     )
 }
