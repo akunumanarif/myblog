f92aaa607e2d7476a99f15c63efc3d1216eb2524
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-13 01:44:06 +0700

    setting all pages route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider, Router } from "react-router-dom";
 import Register from "./pages/Register";
+import Register from "./pages/Login";
+import Register from "./pages/Home";
+import Register from "./pages/Api";
+import Register from "./pages/Single";
+import Register from "./pages/Write";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -9,6 +14,26 @@ const router = createBrowserRouter([
   {
     path: "/register",
     element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/api",
+    element: <Api />,
+  },
+  {
+    path: "/Write",
+    element: <Single />,
+  },
+  {
+    path: "/write",
+    element: <Write />,
   },
 ]);
 
