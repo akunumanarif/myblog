b1fbdfb9acd204ab66bd781cb897bcc04d08a64e
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-13 05:16:08 +0700

    Designing register form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
     return (
-        <div>
-            Halo register
+        <div className='auth'>
+
+            <h1>Register</h1>
+            <form >
+                <input type="text" placeholder='username'/>
+                <input type="password" placeholder='password'/>
+                <button>Register</button>
+                <p>There's an error!</p>
+                <span>Already have an account ? <Link to='/login'>Login</Link></span>
+            </form>
         </div>
     )
 }
