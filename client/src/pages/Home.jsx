67d2dff9a29f3e4e6543b3e95821f624288def55
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-13 16:00:53 +0700

    updating home page design

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -35,7 +35,7 @@ const Home = () => {
                 {posts.map((post) => (
                     <div className="post" key={post.id}>
                         <div className="img">
-                        <img src={`../upload/${post.img}`} alt="" />
+                        <img src={post.img} alt="" />
                     </div>
                     <div className="content">
                     <Link className="link" to={`/post/${post.id}`}>
@@ -46,7 +46,7 @@ const Home = () => {
                     </div>
                     </div>
                 ))}
-      </div>
+            </div>
         </div>
     )
 }
