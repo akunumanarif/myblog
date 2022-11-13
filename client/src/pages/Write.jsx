53b85bb1ada66e168ec0c6b3ee9848c69d7cd824
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-14 01:30:37 +0700

    install react-quill package

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Write = () => {
     return (
-        <div>
-            
+        <div className='add'>
+            <div className="content">
+                <input type="text" placeholder='Title' />
+            </div>
+            <div className="menu">
+                <div className="item">item 1</div>
+                <div className="item">item 2</div>
+            </div>
         </div>
     )
 }
