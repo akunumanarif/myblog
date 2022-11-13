5850f8d847f061a38e5e5c1763f549aac84295a1
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-13 15:48:29 +0700

    designing footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Logo from "../img/logo.png";
 
 const Footer = () => {
     return (
-        <div>
-            Footer Component
-        </div>
+        <footer>
+            <img src={Logo} alt="" />
+            <span>
+                Made with ♥️ and <b>React.js</b>.
+            </span>
+        </footer>
     )
 }
 
