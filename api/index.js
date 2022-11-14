40eeb55472fd0ab043fc4055beee612830d72cfa
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-14 17:20:21 +0700

    install cookie-parser and finish login fnctionalty

--- api/index.js
+++ api/index.js
@@ -2,11 +2,14 @@ import express from "express";
 import authRoutes from "./routes/auth.js";
 // import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
+import cookieParser from "cookie-parser";
 const app = express();
 
 const port = 8800;
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/posts", postRoutes);
 app.use("/api/auth", authRoutes);
 
