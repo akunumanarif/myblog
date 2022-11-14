710003b4a8d64ed31b32bd3215281dee36800632
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-15 00:04:55 +0700

    settingup post in backend

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,13 +1,19 @@
 import express from "express";
-// import { addPost } from "../controllers/post.js";
+import {
+  addPost,
+  getPost,
+  getPosts,
+  updatePost,
+  deletePost,
+} from "../controllers/post.js";
 
 const router = express.Router();
 
-// router.get("/test", addPost);
-// router.get("/", getPosts);
-// router.get("/:id", getPost);
-// router.post("/", addPost);
-// router.delete("/:id", deletePost);
-// router.put("/:id", updatePost);
+router.get("/test", addPost);
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", addPost);
+router.delete("/:id", deletePost);
+router.put("/:id", updatePost);
 
 export default router;
