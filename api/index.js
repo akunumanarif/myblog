10e71e7b2c8fa128088d9a783cc95ee376ecc163
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-14 09:11:29 +0700

    setting up apai routes folder and file

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 import express from "express";
-
+// import authRoutes from "./routes/auth.js";
+// import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 const app = express();
 
 const port = 8800;
 
 app.use(express.json());
+app.use("/api/posts", postRoutes);
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
