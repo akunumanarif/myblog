65e68f8738fcb2bde0fa72116e1d8c48c5bf99d6
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-14 17:08:02 +0700

    creating login page functionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,21 +1,58 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from "axios";
+import React, { useState } from "react";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div className='auth'>
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
 
-            <h1>Login</h1>
-            <form >
-                <input required type="text" placeholder='username'/>
-                <input required type="email" placeholder='email'/>
-                <input required type="password" placeholder='password'/>
-                <button>Login</button>
-                <p>There's an error!</p>
-                <span>Don't have an account ? <Link to='/register'>Sign up</Link></span>
-            </form>
-        </div>
-    )
-}
+  const navigate = useNavigate();
 
-export default Login
+  //   const { login } = useContext(AuthContext);
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/auth/login", inputs);
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+  return (
+    <div className="auth">
+      <h1>Login</h1>
+      <form>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Login</button>
+        {err && <p>{err}</p>}
+        <span>
+          Don't you have an account? <Link to="/register">Register</Link>
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
