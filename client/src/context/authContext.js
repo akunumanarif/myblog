82999f9deba466f8a5a8838fefe27dac10aa268f
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-14 19:07:26 +0700

    Fix store currentUser to localdata in login page

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -1,9 +1,30 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
-export const authContext = createContext();
+export const AuthContext = createContext();
 
-export const authContextProvider = ({ children }) => {
-  const [currentUser, setcurrentUser] = useState(
-    JSON.parse(localStorage.getItem("user") || null)
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+
+  const login = async (inputs) => {
+    const res = await axios.post("/auth/login", inputs);
+    setCurrentUser(res.data);
+  };
+
+  const logout = async (inputs) => {
+    await axios.post("/auth/logout");
+    setCurrentUser(null);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, login, logout }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
