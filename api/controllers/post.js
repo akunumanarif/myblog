749b3ef1d6e35c4eee1413e964eb3f4d6160bba9
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-15 03:04:17 +0700

    Make delete and get posts from cruser fnction

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,4 +1,5 @@
 import { db } from "../db.js";
+import jwt from "jsonwebtoken";
 
 export const getPosts = (req, res) => {
   const q = req.query.cat
@@ -10,9 +11,10 @@ export const getPosts = (req, res) => {
     return res.status(200).json(data);
   });
 };
+
 export const getPost = (req, res) => {
   const q =
-    "SELECT p.id, `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`,`date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
+    "SELECT `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`,`date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
 
   db.query(q, [req.params.id], (err, data) => {
     if (err) return res.status(500).json(err);
@@ -20,12 +22,27 @@ export const getPost = (req, res) => {
     return res.status(200).json(data[0]);
   });
 };
+
 export const addPost = (req, res) => {
   res.json("from controller");
 };
+
 export const deletePost = (req, res) => {
   const token = req.cookies.access_token;
   if (!token) return res.status(401).json("Not Authenticated");
+
+  jwt.verify(token, "jwtkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Invalid Token");
+
+    const postId = req.params.id;
+    const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?";
+
+    db.query(q, [postId, userInfo.id], (err, data) => {
+      if (err) return res.status(403).json("You can only delete your post");
+
+      return res.status(200).json("Post has been deleted");
+    });
+  });
 };
 export const updatePost = (req, res) => {
   res.json("from controller");
