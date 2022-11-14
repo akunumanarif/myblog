236adcdc75a360c621e3f2a5e84ac0deed6683f4
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-15 02:21:19 +0700

    delete post auth functionality

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -6,18 +6,26 @@ export const getPosts = (req, res) => {
     : "SELECT * FROM posts";
 
   db.query(q, [req.query.cat], (err, data) => {
-    if (err) return res.json(err);
+    if (err) return res.status(500).json(err);
     return res.status(200).json(data);
   });
 };
 export const getPost = (req, res) => {
-  res.json("from controller");
+  const q =
+    "SELECT p.id, `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`,`date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
+
+  db.query(q, [req.params.id], (err, data) => {
+    if (err) return res.status(500).json(err);
+
+    return res.status(200).json(data[0]);
+  });
 };
 export const addPost = (req, res) => {
   res.json("from controller");
 };
 export const deletePost = (req, res) => {
-  res.json("from controller");
+  const token = req.cookies.access_token;
+  if (!token) return res.status(401).json("Not Authenticated");
 };
 export const updatePost = (req, res) => {
   res.json("from controller");
