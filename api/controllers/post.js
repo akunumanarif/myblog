729c7f3de7add79b880e2e1ee7f52a5a83f0f02f
M Nu'man Arif H <numanarif87@gmail.com>
2022-11-15 00:23:18 +0700

    fetxhdata from backend api

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,5 +1,14 @@
+import { db } from "../db.js";
+
 export const getPosts = (req, res) => {
-  res.json("from controller");
+  const q = req.query.cat
+    ? "SELECT * FROM posts WHERE cat=?"
+    : "SELECT * FROM posts";
+
+  db.query(q, [req.query.cat], (err, data) => {
+    if (err) return res.json(err);
+    return res.status(200).json(data);
+  });
 };
 export const getPost = (req, res) => {
   res.json("from controller");
